d822a31590777e64978354a17837577b75be9609
metapone <sakamoto52-github@yahoo.com>
2024-08-20 23:52:56 +0700

    Add support for investigators

---
+++ js/data_investigator.js
@@ -0,0 +1,321 @@
+deckData.push({
+	name: "Investigator",
+	cssClass: "investigator",
+	cardsByExpansions: [
+		{
+			expansionID: 0,
+			cards: [
+				{
+					id: 1,
+					name: "Akachi Onyele",
+					traits: ["Gate Closer"],
+				},
+				{
+					id: 2,
+					name: "Charlie Kane",
+					traits: ["Support"],
+				},
+				{
+					id: 3,
+					name: "Diana Stanley",
+					traits: ["Magic"],
+				},
+				{
+					id: 4,
+					name: "Jacqueline Fine",
+					traits: ["Magic", "Support"],
+				},
+				{
+					id: 5,
+					name: "Jim Culver",
+					traits: ["Magic", "Support"],
+				},
+				{
+					id: 6,
+					name: "Leo Anderson",
+					traits: ["Expedition", "All-Rounder"],
+				},
+				{
+					id: 7,
+					name: "Lily Chen",
+					traits: ["Combat", "All-Rounder"],
+				},
+				{
+					id: 8,
+					name: "Lola Hayes",
+					traits: ["All-Rounder", "Support"],
+				},
+				{
+					id: 9,
+					name: "Mark Harrigan",
+					traits: ["Combat"],
+				},
+				{
+					id: 10,
+					name: "Norman Withers",
+					traits: ["Magic", "Gate Closer"],
+				},
+				{
+					id: 11,
+					name: "Silas Marsh",
+					traits: ["All-Rounder"],
+				},
+				{
+					id: 12,
+					name: "Trish Scarborough",
+					traits: ["Research"],
+				},
+			],
+		},
+		{
+			expansionID: 2,
+			cards: [
+				{
+					id: 13,
+					name: "Agnes Baker",
+					traits: ["Magic", "Support"],
+				},
+				{
+					id: 14,
+					name: "Daisy Walker",
+					traits: ["Magic", "Research"],
+				},
+				{
+					id: 15,
+					name: "Finn Edwards",
+					traits: ["Support", "All-Rounder"],
+				},
+				{
+					id: 16,
+					name: "George Barnaby",
+					traits: ["Support"],
+				},
+				{
+					id: 17,
+					name: "Patrice Hathaway",
+					traits: ["Gate Closer"],
+				},
+				{
+					id: 18,
+					name: "Tommy Muldoon",
+					traits: ["Combat"],
+				},
+				{
+					id: 19,
+					name: "Ursula Downs",
+					traits: ["Expedition"],
+				},
+				{
+					id: 20,
+					name: "Wilson Richards",
+					traits: ["Combat"],
+				},
+			],
+		},
+		{
+			expansionID: 3,
+			cards: [
+				{
+					id: 21,
+					name: '"Skids" O\'Toole',
+					traits: ["Combat"],
+				},
+				{
+					id: 22,
+					name: "Marie Lambeau",
+					traits: ["Magic", "Support"],
+				},
+				{
+					id: 23,
+					name: "Tony Morgan",
+					traits: ["Research"],
+				},
+				{
+					id: 24,
+					name: "Zoey Samaras",
+					traits: ["Combat"],
+				},
+			],
+		},
+		{
+			expansionID: 4,
+			cards: [
+				{
+					id: 25,
+					name: "Hank Samson",
+					traits: ["Combat", "Expedition"],
+				},
+				{
+					id: 26,
+					name: "Harvey Walters",
+					traits: ["Support"],
+				},
+				{
+					id: 27,
+					name: "Joe Diamond",
+					traits: ["Research"],
+				},
+				{
+					id: 28,
+					name: "Mandy Thompson",
+					traits: ["Research"],
+				},
+				{
+					id: 29,
+					name: "Minh Thi Phan",
+					traits: ["Support"],
+				},
+				{
+					id: 30,
+					name: "Monterey Jack",
+					traits: ["Expedition"],
+				},
+				{
+					id: 31,
+					name: "Rex Murphy",
+					traits: ["All-Rounder"],
+				},
+				{
+					id: 32,
+					name: "Sister Mary",
+					traits: ["Support"],
+				},
+			],
+		},
+		{
+			expansionID: 5,
+			cards: [
+				{
+					id: 33,
+					name: "Dexter Drake",
+					traits: ["Magic", "Gate Closer"],
+				},
+				{
+					id: 34,
+					name: "Jenny Barnes",
+					traits: ["Support"],
+				},
+				{
+					id: 35,
+					name: "Michael McGlen",
+					traits: ["Combat"],
+				},
+				{
+					id: 36,
+					name: "Wendy Adams",
+					traits: ["Research"],
+				},
+			],
+		},
+		{
+			expansionID: 6,
+			cards: [
+				{
+					id: 37,
+					name: "Amanda Sharpe",
+					traits: ["Research"],
+				},
+				{
+					id: 38,
+					name: "Carolyn Fern",
+					traits: ["Support"],
+				},
+				{
+					id: 39,
+					name: "Darrell Simmons",
+					traits: ["Research"],
+				},
+				{
+					id: 40,
+					name: "Gloria Goldberg",
+					traits: ["Support", "Research"],
+				},
+				{
+					id: 41,
+					name: "Kate Winthrop",
+					traits: ["Research", "Gate Closer"],
+				},
+				{
+					id: 42,
+					name: "Luke Robinson",
+					traits: ["Gate Closer"],
+				},
+				{
+					id: 43,
+					name: "Vincent Lee",
+					traits: ["Support"],
+				},
+				{
+					id: 44,
+					name: "William Yorick",
+					traits: ["Combat"],
+				},
+			],
+		},
+		{
+			expansionID: 7,
+			cards: [
+				{
+					id: 45,
+					name: '"Ashcan" Pete',
+					traits: ["All-Rounder", "Gate Closer"],
+				},
+				{
+					id: 46,
+					name: "Bob Jenkins",
+					traits: ["Support", "All-Rounder"],
+				},
+				{
+					id: 47,
+					name: "Rita Young",
+					traits: ["Combat"],
+				},
+				{
+					id: 48,
+					name: "Roland Banks",
+					traits: ["Research", "Support"],
+				},
+			],
+		},
+		{
+			expansionID: 8,
+			cards: [
+				{
+					id: 49,
+					name: "Agatha Crane",
+					traits: ["Magic", "Research"],
+				},
+				{
+					id: 50,
+					name: "Calvin Wright",
+					traits: ["All-Rounder", "Support"],
+				},
+				{
+					id: 51,
+					name: "Carson Sinclair",
+					traits: ["Support", "All-Rounder"],
+				},
+				{
+					id: 52,
+					name: "Daniela Reyes",
+					traits: ["All-Rounder"],
+				},
+				{
+					id: 53,
+					name: "Father Mateo",
+					traits: ["Gate Closer"],
+				},
+				{
+					id: 54,
+					name: "Preston Fairmont",
+					traits: ["Support"],
+				},
+				{
+					id: 55,
+					name: "Sefina Rousseau",
+					traits: ["Research"],
+				},
+			],
+		},
+	],
+});
